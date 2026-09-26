702c640950855571fe5c52cfa527b074ef693298
Himanshu Gupta <himanshuprihamsam13@gmail.com>
2026-09-26 22:39:30 +0530

    Largest of three numbers

---
+++ largest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the value of a,b and c:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+    {
+        if(a>c)
+        printf("The largest number is:%d",a);
+        else
+        printf("The largest number is:%d",c);
+    }
+    else
+    {
+        if(b>c)
+        printf("The largest number is:%d",b);
+        else
+        printf("The largest number is:%d",c);
+    }
+    return 0;
+}
